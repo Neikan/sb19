925214dc99383938a74bac494c5a5e97f3887915
Neikan <sladkov.e.m@outlook.com>
2023-03-17 16:12:24 +0300

    feat(project): add ffi_bridge to Android

--- lib/consts/translations.dart
+++ lib/consts/translations.dart
@@ -4,6 +4,7 @@ import 'package:app_integrations/consts/keys.dart';
 const labelsHome = <String, String>{
   keyTitle: 'Platform view',
   keyInput: 'Enter text',
+  keyLength: 'Length:',
   keyOutput: 'Entered text:',
   keyButton: 'Display text',
 };
